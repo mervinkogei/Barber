5bff1b6b9d0540d435dc391e05fba145f7094ceb
mervinkogei <vincent.kogei@yahoo.com>
2019-12-13 12:25:51 +0300

    add Animation transition

--- app/src/main/java/com/example/barber/Splash.java
+++ app/src/main/java/com/example/barber/Splash.java
@@ -3,12 +3,24 @@ package com.example.barber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+
+    private TextView tvWelcome;
+    private ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvWelcome = (TextView) findViewById(R.id.tvWelcome);
+        ivLogo = (ImageView) findViewById(R.id.ivLogo);
+
+        Animation mytransition = AnimationUtils.loadAnimation(this,R.anim.mytransition);
     }
 }

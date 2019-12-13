0f8767a96a1b18f421c7ae7a847fb62f499e94a9
mervinkogei <vincent.kogei@yahoo.com>
2019-12-13 14:22:58 +0300

    Resize the Image view on the Splash Screen Display and the Text

--- app/src/main/java/com/example/barber/Splash.java
+++ app/src/main/java/com/example/barber/Splash.java
@@ -2,6 +2,7 @@ package com.example.barber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,6 +14,7 @@ public class Splash extends AppCompatActivity {
     private TextView tvWelcome;
     private ImageView ivLogo;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,24 @@ public class Splash extends AppCompatActivity {
 
         tvWelcome = (TextView) findViewById(R.id.tvWelcome);
         ivLogo = (ImageView) findViewById(R.id.ivLogo);
+      final Intent intent = new Intent(this,MainActivity.class);
 
         Animation mytransition = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tvWelcome.startAnimation(mytransition);
+        ivLogo.startAnimation(mytransition);
+        final Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }

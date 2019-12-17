e5add1b3e1bf8adc69a6ec749f8ced05bad92b3e
mervinkogei <vincent.kogei@yahoo.com>
2019-12-17 08:40:07 +0300

    Add Login and Bookings Activity

---
+++ app/src/main/java/com/example/barber/Login.java
@@ -0,0 +1,109 @@
+package com.example.barber;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Info = (TextView) findViewById(R.id.tvinfo);
+        Login = (Button) findViewById(R.id.btnlogin);
+        userRegistration = (TextView) findViewById(R.id.tvRegister);
+
+        Info.setText("No of Attempts remaining: 5");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user= firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(Login.this,Bookings.class));
+        }
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,MainActivity.class));
+            }
+        });
+    }
+
+    public void validate(String userName, String userPassword) {
+
+        progressDialog.setMessage("You can Check a quick tutorial on Login Demo");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+//                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    checkEmailVerification();
+                }else {
+                    Toast.makeText(Login.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                    counter --;
+
+                    Info.setText("no of Attempts Remaining: " + counter);
+                    if(counter == 0){
+                        progressDialog.dismiss();
+                        Login.setEnabled(false);
+                    }
+                }
+
+            }
+        });
+
+    }
+    private void checkEmailVerification(){
+        FirebaseUser firebaseUser = firebaseAuth.getInstance().getCurrentUser();
+        Boolean emailflag = firebaseUser.isEmailVerified();
+
+        if(emailflag){
+            finish();
+            startActivity(new Intent( Login.this,MainActivity.class));
+            Toast.makeText(this,"Verify your email account",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
+    }
+}
--- app/src/main/java/com/example/barber/MainActivity.java
+++ app/src/main/java/com/example/barber/MainActivity.java
@@ -7,8 +7,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -29,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.db){
+            Toast.makeText(this,"Dashboard",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.prof){
+            Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.cal){
+            Toast.makeText(this,"Calendar",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.book){
+            Toast.makeText(this,"Bookings",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.pay){
+            Toast.makeText(this,"Payment",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.set){
+            Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+        }
+        if(id == R.id.logout){
+            Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show();
+        }
+        return  false;
     }
 }
